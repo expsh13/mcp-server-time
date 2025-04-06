bc5fade4f6d4e774bc4b5d33978d099cb3fc66eb
Satoki Hata <satokihata@gmail.com>
2025-04-06 15:37:58 +0900

    add mcp-server

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+
+// MCPサーバーを作成
+const server = new McpServer({
+  name: "時間表示サーバー",
+  version: "1.0.0",
+});
